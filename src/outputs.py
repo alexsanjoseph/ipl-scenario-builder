5685b8bca41030d29315be9795228f5532e9f60a
alexsanjoseph <alexsanjoseph@gmail.com>
2022-05-10 13:22:59 +0530

    Automated data update

--- src/outputs.py
+++ src/outputs.py
@@ -8,12 +8,12 @@ def write_output_in_ipl_scenario_format(filtered_fixtures, standings) -> None:
     transposed_standings = standings.set_index("symbol").T
     transposed_standings.columns = transposed_standings.columns.str.lower()
     print("writing standings to file")
-    transposed_standings.to_json("standings.json", indent=2)
+    transposed_standings.to_json("data/current_standings.json", indent=2)
 
     filtered_fixtures_write = filtered_fixtures \
         .assign(t1=filtered_fixtures['for'].str.lower(), t2=filtered_fixtures['against'].str.lower()) \
         .assign(win="") \
         .drop(["datetime", "for", "against"], axis=1) \
         .T
-    filtered_fixtures_write.to_json("fixtures.json", indent=2)
+    filtered_fixtures_write.to_json("data/filtered_fixtures.json", indent=2)
     print("writing fixtures to file")
